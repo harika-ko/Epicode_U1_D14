4c3a9e8034afe6bbc55c0c02de826181230e6b49
Harika_Kommuri <harikakommuri35@gmail.com>
2022-05-12 14:43:56 +0200

    Added CSS

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ function displayNumbers() {
     for (let i = 1; i <= numbers; i++) {
         let numNode = document.createElement("div")
         numNode.innerText = i
+        numNode.classList.add('numbers')
         numContainer.appendChild(numNode)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+function executeOnLoad() {
+    displayNumbers()
+}
+
+window.onload = executeOnLoad

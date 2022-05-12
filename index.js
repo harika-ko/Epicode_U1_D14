2100aa7bd7f7900b952fddbeb236ee96e12b6d14
Harika_Kommuri <harikakommuri35@gmail.com>
2022-05-12 16:21:22 +0200

    Highlighted Random number on Bingo Board
    Also added some CSS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const numbers = 76
 
+
 function displayNumbers() {
     let numContainer = document.getElementById("numbers-container")
 
@@ -11,15 +12,20 @@ function displayNumbers() {
     }
 }
 
+function randomizeNumber() {
+    let randomNumber = Math.floor(Math.random() * 76) + 1
+    return randomNumber
+}
 
-
-
-
-
-
-
-
-
+function selectNumber() {
+    let randomfunc = randomizeNumber()
+    let newNumNode = document.querySelectorAll(".numbers")
+    for (let i = 1; i < newNumNode.length; i++) {
+        if (randomfunc === parseInt(newNumNode[i].innerText)) {
+            newNumNode[i].classList.add('selected-number')
+        }
+    }
+}
 
 function executeOnLoad() {
     displayNumbers()
